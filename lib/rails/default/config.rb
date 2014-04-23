250e113427fc9cb51284a26bf3cdaee02c5f1077
Daniel Fone <daniel@fone.net.nz>
2014-04-23 14:35:36 +1200

    bundle gem skeleton

---
+++ lib/rails/default/config.rb
@@ -0,0 +1,9 @@
+require "rails/default/config/version"
+
+module Rails
+  module Default
+    module Config
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/default/config/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Default
+    module Config
+      VERSION = "0.0.1"
+    end
+  end
+end
